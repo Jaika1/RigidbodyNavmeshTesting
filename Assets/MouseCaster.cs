bb84c5a6e8cf82920469c3aa2383331d4af3b969
Jaika1 <jackroennfeldtnospam@gmail.com>
2020-11-05 22:45:12 +1030

    could be worse

---
+++ Assets/MouseCaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCaster : MonoBehaviour
+{
+    public MovementScript MS;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit rh;
+            if (Physics.Raycast(r, out rh))
+            {
+                MS.RunPath(rh.point);
+            }
+        }
+    }
+}
---
+++ Assets/MovementScript.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody rBody;
+    Vector3[] crossPoints = new Vector3[0];
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        rBody.freezeRotation = true;
+        rBody.isKinematic = true;
+    }
+
+    void Update()
+    {
+        if (crossPoints.Length == 0) return;
+
+        if (Vector3.Distance(transform.position, crossPoints[0]) < 0.1f)
+        {
+            crossPoints = crossPoints.Skip(1).ToArray();
+        }
+        else
+        {
+            transform.LookAt(crossPoints[0]);
+            Vector3 moveVect = crossPoints[0] - transform.position;
+            moveVect.Normalize();
+            moveVect *= 16.0f * Time.deltaTime;
+            rBody.MovePosition(transform.position + moveVect);
+        }
+    }
+
+    public void RunPath(Vector3 destination)
+    {
+        NavMeshPath path = new NavMeshPath();
+        if (NavMesh.CalculatePath(transform.position, destination, -1, path))
+        {
+            crossPoints = path.corners;
+            for (int i = 0; i < crossPoints.Length; ++i)
+            {
+                crossPoints[i] = new Vector3(crossPoints[i].x, 1, crossPoints[i].z);
+            }
+        }
+    }
+}
